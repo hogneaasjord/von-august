4381dde4321cbbe527e6e30d56f8e559fe4ffa75
hogneaasjord <hogne.aasjord@gmail.com>
2021-11-18 18:56:17 +0100

    centered and responsive

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="fp-img">
-      <a href="https://ffm.to/blaoyd"><img src="blaoyd.jpg" alt=""/></a>
+      <a href="https://ffm.to/blaoyd"><img height="500px" src="blaoyd.jpg" alt=""/></a>
     </div>
   );
 }
